fdc7a78d2b27e023d39b26905978be37594a28ee
GustavoCaso <gustavocaso@gmail.com>
2016-04-10 17:13:02 +0200

    Added seeds for better development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+100.times do
+  ActiveRecord::Base.transaction do
+    password = Faker::Internet.password
+    user_name = Faker::Internet.user_name
+    email = "#{user_name}@gmail.com"
+    user = User.create!(user_name: user_name, email: email, password: password, password_confirmation: password)
+    20.times do
+      user.messages.create!(content: Faker::Lorem.characters(Random.rand(20..159)), created_at: Faker::Time.between(DateTime.now - 100, DateTime.now))
+    end
+  end
+end
